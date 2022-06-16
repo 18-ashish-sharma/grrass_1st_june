479d9276997e6bf5c9223a14fe2bc9a2223b43e2
Your Name <hudsasharma10@gmail.com>
2022-06-16 09:30:47 +0530

    Adding more

--- day10-React-1/grass-1/src/App.js
+++ day10-React-1/grass-1/src/App.js
@@ -1,25 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Hello, Bye } from './funciton';
-import Ok from './funciton';  
+import Ok from './funciton';
+import Student from './New';
 
 export default function App() {
-  // Data structure and algorithims -> DSA
-  let arr = [10,2,30,4,5]
-  function sorty(arr) {
-    for(var i=0; i<arr.length; i++) {
-      for(var j=i+1; j<arr[i].length; j++){
-        arr[i]> arr[j]
-        // 1     arr[1] = 2
-      }
-    }
-  }
   return (
     <div className="App">
       Ashish
       <Hello /> 
       <Bye />
       <Ok />
+      <Student />
+      {/* How to call functions in react which is returning HTML content */}
+      {/* <Topbar /> */}
       {/*  self closing tag */}
     </div>
   );
---
+++ day10-React-1/grass-1/src/New.js
@@ -0,0 +1,10 @@
+function Student(){
+    return (
+        <div className="student">
+            <h1>Hello Hi How are you</h1>
+            <p>Parameters</p>
+        </div>
+    )
+}
+
+export default Student;
